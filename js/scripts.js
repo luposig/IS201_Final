8c5b075853c9db4315a8bb4ce8291d92b25fff75
Herman Yurevich <yurevich.herman@gmail.com>
2025-06-15 18:53:24 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+ < script type = 'text/javascript' >
+     var divElement = document.getElementById('viz1750030769082');
+ var vizElement = divElement.getElementsByTagName('object')[0];
+ vizElement.style.width = '1600px';
+ vizElement.style.height = '7277px';
+ var scriptElement = document.createElement('script');
+ scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+ vizElement.parentNode.insertBefore(scriptElement, vizElement); <
+ /script>
